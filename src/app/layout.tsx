bb33db7a6edea3c0363bf4f54ae6b3d23f267bbb
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-02 19:26:42 +0700

    Feat: Configure Theme Providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Providers from "@/components/providers";
+import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
@@ -11,8 +13,18 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html
+        lang="en"
+        className={cn(
+          "bg-white text-slate-900 dark:text-slate-50 antialiased",
+          inter.className
+        )}
+      >
+        <body className="min-h-screen antialiased bg-slate-50 dark:bg-slate-900">
+          <Providers>
+            <main>{children}</main>
+          </Providers>
+        </body>
       </html>
     </ClerkProvider>
   );
