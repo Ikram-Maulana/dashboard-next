f574a1bba75eb847d1ec3f5a3ac1a1e10216a1e7
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-03 07:19:31 +0700

    Feat: Update Clerk Auth
    Update:
    - Hidden register
    - Delete register page

--- src/app/(auth)/login/[[...login]]/page.tsx
+++ src/app/(auth)/login/[[...login]]/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { SignIn } from "@clerk/nextjs";
 import { ChevronLeftIcon } from "lucide-react";
+import { Metadata } from "next";
 import Link from "next/link";
+import { useEffect } from "react";
+
+export const metadata: Metadata = {
+  title: "Dashboard Next | Login",
+  description: "Dashboard Next is a project learning how to use Clerk.dev",
+};
 
 export default function Page() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const footer = document.querySelector(".cl-footer");
+      if (footer) {
+        footer.remove();
+        clearInterval(interval);
+      }
+    }, 100);
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="flex flex-col items-start gap-4">
@@ -13,7 +32,14 @@ export default function Page() {
             Kembali ke Beranda
           </Link>
         </Button>
-        <SignIn />
+        <SignIn
+          appearance={{
+            elements: {
+              footer: "hidden",
+              card: "m-0",
+            },
+          }}
+        />
       </div>
     </div>
   );
