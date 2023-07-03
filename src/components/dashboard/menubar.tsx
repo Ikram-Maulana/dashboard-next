f063ebcde68ab903cd1d39441e62f3bc9b41bc41
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-03 07:20:31 +0700

    Feat: Update Menubar Component

--- src/components/dashboard/menubar.tsx
+++ src/components/dashboard/menubar.tsx
@@ -1,12 +1,14 @@
 import ThemeToggle from "@/components/theme-toggle";
 import { Button } from "@/ui/button";
+import { SignOutButton, SignedIn, auth } from "@clerk/nextjs";
 import { ArrowUpRightIcon } from "lucide-react";
 import Link from "next/link";
-import { FC } from "react";
 
-const Menubar: FC = () => {
+const Menubar = () => {
+  const { orgRole } = auth();
+
   return (
-    <div className="flex items-center justify-center gap-2 mt-6">
+    <div className="flex flex-wrap items-center justify-center gap-2 mt-6">
       <ThemeToggle />
       <Button variant="secondary" asChild>
         <Link href="/">
@@ -14,6 +16,22 @@ const Menubar: FC = () => {
           <ArrowUpRightIcon className="w-6 h-6 mr-2" />
         </Link>
       </Button>
+      <Button variant="secondary" asChild>
+        <Link href="/dashboard">Dashboard</Link>
+      </Button>
+      {orgRole === "admin" && (
+        <Button variant="secondary" asChild>
+          <Link href="/dashboard/sensitive">Sensitive</Link>
+        </Button>
+      )}
+      <Button variant="secondary" asChild>
+        <Link href="/dashboard/member">Member</Link>
+      </Button>
+      <SignedIn>
+        <Button variant="destructive" asChild>
+          <SignOutButton />
+        </Button>
+      </SignedIn>
     </div>
   );
 };
--- src/components/menubar.tsx
+++ src/components/menubar.tsx
@@ -1,12 +1,12 @@
 import ThemeToggle from "@/components/theme-toggle";
 import { Button } from "@/ui/button";
-import { SignedIn, SignedOut, useSession } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { FC } from "react";
 
 const Menubar: FC = () => {
   return (
-    <div className="flex items-center justify-center gap-2 mt-6">
+    <div className="flex flex-wrap items-center justify-center gap-2 mt-6">
       <ThemeToggle />
       <Button variant="secondary" asChild>
         <Link href="/">Home</Link>
@@ -22,6 +22,9 @@ const Menubar: FC = () => {
       <SignedIn>
         <Button variant="secondary" asChild>
           <Link href="/dashboard">Dashboard</Link>
+        </Button>
+        <Button variant="destructive" asChild>
+          <SignOutButton />
         </Button>
       </SignedIn>
     </div>
