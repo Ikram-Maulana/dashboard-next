b4c090ffe33d4526d4a367eee1c0735fa5d28c51
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-04 13:59:46 +0700

    Feat: Add Detail Membership API

---
+++ src/app/api/dashboard/membership/detail/[id]/route.ts
@@ -0,0 +1,52 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export const GET = async (
+  req: NextRequest,
+  {
+    params,
+  }: {
+    params: {
+      id: number;
+    };
+  }
+) => {
+  const { userId } = auth();
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+  const id = params.id;
+
+  try {
+    const membership = await db.membership.findUnique({
+      where: {
+        id: parseInt(id.toString()),
+      },
+    });
+
+    return NextResponse.json(
+      {
+        error: null,
+        data: membership,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        {
+          error: error.issues,
+          data: null,
+        },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      { error: "Internal server error", data: null },
+      { status: 500 }
+    );
+  }
+};
