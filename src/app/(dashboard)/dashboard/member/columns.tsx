222cc481771deb500dcde804374e5ace5a1d62b3
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-04 10:20:19 +0700

    Feat: Implement Delete Membership

--- src/app/(dashboard)/dashboard/member/columns.tsx
+++ src/app/(dashboard)/dashboard/member/columns.tsx
@@ -1,6 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { deleteMembership } from "@/helpers/delete-membership";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/ui/alert-dialog";
+import { Button } from "@/ui/button";
+import { useToast } from "@/ui/use-toast";
+import { LoaderIcon } from "lucide-react";
+import { useEffect } from "react";
 import { z } from "zod";
 
 const createActionsColumnType = z.object({
@@ -31,6 +48,30 @@ export const columns = [
     header: "Actions",
     cell: ({ row }: ActionsColumnType) => {
       const member = row.original;
+      const { isSuccess, mutate, isLoading, isError } = deleteMembership();
+      const { toast } = useToast();
+
+      const onDeleteHandler = (id: number) => {
+        mutate(id);
+      };
+
+      useEffect(() => {
+        if (isSuccess) {
+          toast({
+            title: "Membership deleted",
+            description: `Membership ${member.name} deleted successfully`,
+          });
+        }
+
+        if (isError) {
+          toast({
+            variant: "destructive",
+            title: "Error",
+            description: "Something went wrong",
+          });
+        }
+      }, [isSuccess, isError, toast]);
+
       return (
         <div className="flex items-center space-x-2">
           <Button
@@ -42,15 +83,35 @@ export const columns = [
           >
             Edit
           </Button>
-          <Button
-            size="sm"
-            variant="outline"
-            onClick={() => {
-              alert(`Delete ${member.name}`);
-            }}
-          >
-            Delete
-          </Button>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button size="sm" variant="outline">
+                {isLoading ? (
+                  <LoaderIcon className="w-4 h-4 mr-2 animate-spin" />
+                ) : null}
+                Delete
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  onClick={() => {
+                    onDeleteHandler(parseInt(member.id));
+                  }}
+                >
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </div>
       );
     },
